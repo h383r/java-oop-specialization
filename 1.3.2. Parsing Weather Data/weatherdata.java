be10d0cd277f2e27ca4b4d8496aa2c8b5984d6ea
h3b3r <heber.alcorta@gmail.com>
2021-04-15 20:59:47 -0300

    commit

--- 1.3.2. Parsing Weather Data/weatherdata.java
+++ 1.3.2. Parsing Weather Data/weatherdata.java
@@ -1,7 +1,3 @@
-import edu.duke.*;
-import org.apache.commons.csv.*;
-import java.io.*;
-/*
 import java.io.File;
 
 import org.apache.commons.csv.CSVParser;
@@ -9,11 +5,67 @@ import org.apache.commons.csv.CSVRecord;
 
 import edu.duke.DirectoryResource;
 import edu.duke.FileResource;
-*/
 
 public class weatherdata {
     
-    // returns the CSVRecord with the coldest temperature in the file
+    public CSVRecord hottestHourInFile (CSVParser parser) {
+        
+        CSVRecord largestSoFar = null;
+        
+        for (CSVRecord currentRow : parser) {
+            
+            if (largestSoFar == null) {
+                largestSoFar = currentRow;
+            }
+            else {
+                double currentTemp = Double.parseDouble(currentRow.get("TemperatureF"));
+                double largestTemp = Double.parseDouble(largestSoFar.get("TemperatureF"));
+                
+                if (currentTemp > largestTemp) {
+                    largestSoFar = currentRow;
+                }
+            }
+        }
+        return largestSoFar;
+    }
+    
+    public void hottestHourInFileTest () {
+        FileResource fr = new FileResource("data/nc_weather/2015/weather-2015-01-02.csv");
+        CSVRecord largest = hottestHourInFile(fr.getCSVParser());
+        System.out.println("hottest temperature was " + largest.get("TemperatureF") + " at " + largest.get("TimeEST"));
+    }
+    
+    public CSVRecord hottestHourInManyFiles () {
+        
+        CSVRecord largestSoFar = null;
+        DirectoryResource dr = new DirectoryResource();
+        
+        for (File f : dr.selectedFiles()) {
+            
+            FileResource fr = new FileResource(f);
+            CSVRecord currentRow = hottestHourInFile(fr.getCSVParser());
+            
+            if (largestSoFar == null) {
+                largestSoFar = currentRow;
+            }
+            else {
+                double currentTemp = Double.parseDouble(currentRow.get("TemperatureF"));
+                double largestTemp = Double.parseDouble(largestSoFar.get("TemperatureF"));
+                
+                if (currentTemp > largestTemp) {
+                    largestSoFar = currentRow;
+                }
+            }
+        }
+        return largestSoFar;
+    }
+    
+    public void hottestHourInManyFilesTest () {
+        CSVRecord largest = hottestHourInManyFiles();
+        System.out.println("hottest temperature was " + largest.get("TemperatureF") + " at " + largest.get("DateUTC"));
+    }
+    
+    // Return the CSVRecord with the coldest temperature in the file
     public CSVRecord coldestHourInFile (CSVParser parser) {
     
         CSVRecord coldest = null;
@@ -45,112 +97,63 @@ public class weatherdata {
         System.out.println("coldest temperature was " + coldest.get("TemperatureF") + " at " + coldest.get("TimeEST"));
     }
 
+    // Return the name of the file from selected files that has the coldest temperature
     public String fileWithColdestTemperature () {
         
         DirectoryResource dr = new DirectoryResource();
-        CSVRecord coldest = null;
-        String filename = null;
+        String filename = "";
 
-        
+        Double coldestTemperature = null;
 
+        // For each file in directory resource
+        for (File currentFile : dr.selectedFiles()) {
 
+            FileResource fr = new FileResource(currentFile);
+            CSVParser parser = fr.getCSVParser();
+            CSVRecord coldestHourInFile = coldestHourInFile(parser);
 
+            // Coldest temperature in file
+            double coldestTemperatureInFile = Double.parseDouble(coldestHourInFile.get("TemperatureF"));
 
-
+            if (filename == "") {
+                filename = currentFile.getPath();
+                coldestTemperature = coldestTemperatureInFile;
+            }
+            else if (coldestTemperature > coldestTemperatureInFile) {
+                filename = currentFile.getPath();
+                coldestTemperature = coldestTemperatureInFile;
+            }
+        }
+        System.out.println(filename);
         return filename;
     }
 
+    public void fileWithColdestTemperatureTest() {
 
-     
-    /*
-        comentar:
-        CSVRecord =>
-        getCSVParser =>
-    */
+        String filename = fileWithColdestTemperature();
+        FileResource fr = new FileResource(filename);
+        CSVParser parser = fr.getCSVParser();
+        CSVRecord coldestHourInFile = coldestHourInFile(parser);
 
+        System.out.println("Coldest day was in file " + filename);
+        System.out.println("Coldest temperature on that day was " + coldestHourInFile.get("TemperatureF"));
+        System.out.println("All the Temperatures on the coldest day were:");
 
-    public CSVRecord hottestHourInFile (CSVParser parser) {
-        
-        CSVRecord largestSoFar = null;
-        
-        for (CSVRecord currentRow : parser) {
-            
-            if (largestSoFar == null) {
-                largestSoFar = currentRow;
-            }
-            else {
-                double currentTemp = Double.parseDouble(currentRow.get("TemperatureF"));
-                double largestTemp = Double.parseDouble(largestSoFar.get("TemperatureF"));
-                
-                if (currentTemp > largestTemp) {
-                    largestSoFar = currentRow;
-                }
-            }
+        // Restart parser
+        parser = fr.getCSVParser();
+
+        for (CSVRecord row : parser) {
+            System.out.println(row.get("DateUTC") + ": " + row.get("TemperatureF"));
         }
-        return largestSoFar;
     }
-    
-    public void hottestHourInFileTest () {
-        FileResource fr = new FileResource("data/nc_weather/2015/weather-2015-01-02.csv");
-        CSVRecord largest = hottestHourInFile(fr.getCSVParser());
-        System.out.println("hottest temperature was " + largest.get("TemperatureF") + " at " + largest.get("TimeEST"));
-    }
-
-    public CSVRecord hottestHourInManyFiles () {
-
-        CSVRecord largestSoFar = null;
-        DirectoryResource dr = new DirectoryResource();
-
-        for (File f : dr.selectedFiles()) {
-
-            FileResource fr = new FileResource(f);
-            CSVRecord currentRow = hottestHourInFile(fr.getCSVParser());
-            
-            if (largestSoFar == null) {
-                largestSoFar = currentRow;
-            }
-            else {
-                double currentTemp = Double.parseDouble(currentRow.get("TemperatureF"));
-                double largestTemp = Double.parseDouble(largestSoFar.get("TemperatureF"));
-                
-                if (currentTemp > largestTemp) {
-                    largestSoFar = currentRow;
-                }
-            }
-        }
-        return largestSoFar;
-    }
-    
-    public void hottestHourInManyFilesTest () {
-        CSVRecord largest = hottestHourInManyFiles();
-        System.out.println("hottest temperature was " + largest.get("TemperatureF") + " at " + largest.get("DateUTC"));
-    }
-
-    /*
-    public CSVRecord getLargestOfTwo (CSVRecord currentRow, CSVRecord largestSoFar) {
-
-        if (largestSoFar == null) {
-            largestSoFar = currentRow;
-        }
-        else {
-            double currentTemp = Double.parseDouble(currentRow.get("TemperatureF"));
-            double largestTemp = Double.parseDouble(largestSoFar.get("TemperatureF"));
-            
-            if (currentTemp > largestTemp) {
-                largestSoFar = currentRow;
-            }
-        }
-        return largestSoFar;
-    }
-    */
-
 
     public static void main (String[] args) {
         weatherdata weatherdata = new weatherdata();
         
-        weatherdata.hottestHourInFileTest();
+        //weatherdata.hottestHourInFileTest();
         //weatherdata.coldestHourInFileTest();
         //weatherdata.hottestHourInManyFilesTest();
-           
+        weatherdata.fileWithColdestTemperatureTest();
+
     }    
 }
