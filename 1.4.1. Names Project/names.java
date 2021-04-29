555ec6b08ca521e0a629d989b5af51fd11063375
h3b3r <heber.alcorta@gmail.com>
2021-04-28 21:06:08 -0300

    commit

--- 1.4.1. Names Project/names.java
+++ 1.4.1. Names Project/names.java
@@ -53,8 +53,42 @@ public class names {
 
     //ASSIGNMENT ===============================================================
 
-    public void getRank(int year, String name, String gender) {
-        
+    /**
+     * Rank of the name in the file for the given gender
+     * @param year
+     * @param name
+     * @param gender
+     * @return Rank of the name. (-1 if the name is not in the file)
+     */
+    public int getRank(int year, String name, String gender) {
+        FileResource fr = new FileResource("data/us_babynames_by_year/yob" + year + ".csv");
+        CSVParser parser = fr.getCSVParser(false);
+        int rank = 0;
+
+        for (CSVRecord currentRow : parser) {
+
+            
+        }
+
+
+
+        /*
+        FileResource fr = new FileResource();
+        CSVParser parser = fr.getCSVParser(false); // False = no header row
+
+        for (CSVRecord currentRow : parser) {
+
+            int numBorn = Integer.parseInt(currentRow.get(2));
+
+            if(numBorn <= 100) {
+                System.out.print("Name: " + currentRow.get(0));
+                System.out.print(" Gender: " + currentRow.get(1));
+                System.out.print(" Num Born: " + currentRow.get(2));
+                System.out.print("\n");
+            }
+        }
+        */
+        return rank;
     }
 
 
@@ -84,7 +118,7 @@ public class names {
         names names = new names();
 
         names.printNames();
-        names.totalBirths();
+        //names.totalBirths();
 
     }
 }
