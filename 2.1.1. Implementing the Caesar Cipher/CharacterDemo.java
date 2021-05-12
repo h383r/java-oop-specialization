5fcf1c514abdcf77a1b29b78d60c2d467477aeab
h3b3r <heber.alcorta@gmail.com>
2021-05-11 21:36:36 -0300

    commit

--- 2.1.1. Implementing the Caesar Cipher/CharacterDemo.java
+++ 2.1.1. Implementing the Caesar Cipher/CharacterDemo.java
@@ -1,10 +1,12 @@
 public class CharacterDemo {
 
     /**
-     * 
+     * Check if character is a digit, alphabetic or hashtag
      */
     public void digitTest() {
+
         String test = "ABCabc0123456789';#!";
+        
         for(int k=0; k < test.length(); k++){
             char ch = test.charAt(k);
             if (Character.isDigit(ch)){
@@ -20,10 +22,12 @@ public class CharacterDemo {
     }
     
     /**
-     * 
+     * UpperCase LowerCase conversion Test
      */
     public void conversionTest(){
+        
         String test = "ABCDEFabcdef123!#";
+        
         for(int k=0; k < test.length(); k++){
             char ch = test.charAt(k);
             char uch = Character.toUpperCase(ch);
--- 2.1.1. Implementing the Caesar Cipher/WorldPlay.java
+++ 2.1.1. Implementing the Caesar Cipher/WorldPlay.java
@@ -2,20 +2,12 @@ public class WorldPlay {
     
     /**
      * Returns true if ch is a vowel
-     * @param ch
-     * @return
      */
     public boolean isVowel(char ch) {
 
         ch = Character.toLowerCase(ch);
-        String vowels = "aeiou";
-        boolean isVowel = false;
 
-        for (int i = 0; i < vowels.length(); i++) {
-            if vowel.indexOf()
-        }
-
-        if (ch == "a" || ch == "e" || ch == "i" || ch == "o" || ch == "u") {
+        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
             return true;
         }
         return false;
@@ -23,35 +15,56 @@ public class WorldPlay {
 
     /**
      * Return a String that is the string  with all the vowels replaced by ch
-     * @param phrase
-     * @param ch
-     * @return
      */
     public String replaceVowels(String phrase, char ch) {
 
+        StringBuilder sb = new StringBuilder(phrase);
 
+        for (int i = 0; i < sb.length(); i++ ) {
+            char currentCharacter = sb.charAt(i);
+            int currentIndex = phrase.indexOf(currentCharacter, i);
 
-        return "asd";
+            if(isVowel(currentCharacter)) {
+                sb.setCharAt(currentIndex, ch);
+            }
+        }
+        return sb.toString();
     }
 
     /**
-     * Return a String that is teh string phrase but with the char replaced by:
+     * Return a String that is the string phrase but with the char replaced by:
      * "*" if it is in an odd number location
      * "+" if it is in an even number location
-     * @param phrase
-     * @param ch
-     * @return
      */
     public String emphatize(String phrase, char ch) {
+        
+        ch = Character.toLowerCase(ch);
+        StringBuilder sb = new StringBuilder(phrase);
 
+        for (int i = 0; i < sb.length(); i++) {
 
-        return "asd";
+            char currentCharacter = sb.charAt(i);
+            int currentIndex = phrase.indexOf(currentCharacter, i);
+
+            if (currentCharacter == ch && ((currentIndex+1) % 2 == 0)) {
+                sb.setCharAt(currentIndex, '+');
+            }
+
+            if (currentCharacter == ch && ((currentIndex+1) % 2 != 0)) {
+                sb.setCharAt(currentIndex, '*');
+            }
+
+        }
+        return sb.toString();
     }
 
     public static void main(String[] args) {
         WorldPlay WorldPlay = new WorldPlay();
-        System.out.println(WorldPlay.isVowel("a"));
 
+        //Test Cases
+        System.out.println(WorldPlay.isVowel('a'));
+        System.out.println(WorldPlay.replaceVowels("qwerty", 'X'));
+        System.out.println(WorldPlay.emphatize("xxxxxxxxxxxxxxxx", 'x'));
 
     }
 }
