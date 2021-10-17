03eaa6e61d0e78f140b7f4d6c45faa1f65fbc9aa
h383r <79162598+h383r@users.noreply.github.com>
2021-10-17 15:56:59 -0300

    commit

--- 2.3.1. Reading Log Files/LogAnalyzer.java
+++ 2.3.1. Reading Log Files/LogAnalyzer.java
@@ -18,6 +18,7 @@ public class LogAnalyzer {
      * @param filename
      */
     public void readFile(String filename) {
+
         FileResource fr = new FileResource(filename);
         for (String line: fr.lines()) {
             LogEntry le = WebLogParser.parseEntry(line);
@@ -26,11 +27,108 @@ public class LogAnalyzer {
     }
     
     /**
-     * Prints all records
+     * Prints all records in the Log
      */
     public void printAll() {
+
         for (LogEntry le : records) {
             System.out.println(le);
         }
     }
+
+    /**
+     * Return an integer representing the number of unique IP addresses.
+     */
+    public int countUniqueIPs () {
+
+        ArrayList<String> uniqueIPs = new ArrayList<String>();
+        for (LogEntry log: records) {
+            String ip = log.getIpAddress();
+            if (!uniqueIPs.contains(ip)) {
+                uniqueIPs.add(ip);
+            }
+        }
+        return uniqueIPs.size();   
+    }
+
+    /**
+     * Examine all the web log entries in records
+     * Print those LogEntrys that have a status code greater than num
+     */
+    public void printAllHigherThanNum (int num) {
+
+        for (LogEntry log: records) {
+            if (log.getStatusCode() > num) {
+                System.out.println(log);
+            }
+        }
+    }
+    
+    /**
+     * Accesses the web logs in records and returns an ArrayList
+     * of Strings of unique IP addresses that had access on the
+     * given day
+     * @param someday Date in format “MMM DD” (
+     * MMM is the first three characters of the month name.
+     * DD is the day in two digits
+     * Examples are “Dec 05” and “Apr 22”.)
+     */
+    public ArrayList<String> uniqueIPVisitsOnDay (String someday) {
+
+        ArrayList<String> addresses = new ArrayList<String>();
+
+        for (LogEntry log: records) {
+            String date = log.getAccessTime().toString();
+            String address = log.getIpAddress();
+            if (date.contains(someday) && !addresses.contains(address)) {
+                addresses.add(address);
+            }
+        }
+        return addresses;
+    }
+
+    /**
+     * Returns the number of unique IP addresses in records that have a status
+     * code in the range from low to high, inclusive.
+     */
+    public int countUniqueIPsInRange (int low, int high) {
+        
+        ArrayList<String> addresses = new ArrayList<String>();
+
+        for(LogEntry log: records) {
+            String address = log.getIpAddress();
+            int status = log.getStatusCode();
+            if (!addresses.contains(address) && status >= low && status <= high) {
+                addresses.add(address);
+            }
+        }
+        return addresses.size();
+    }
+
+    /**
+     * Read in web log entries, but only put a LogEntry in
+     * records if it hasn’t seen the LogEntry's status
+     * code before. Thus, the entries in records would 
+     * have unique status code
+     */
+    public void readFileWithUniqueStatus (String filename) {
+        FileResource fr = new FileResource(filename);
+        for (String line : fr.lines()) {
+            LogEntry le = WebLogParser.parseEntry(line);
+            if (! records.contains(le)) {
+                records.add(le);
+            }
+        }
+
+        /*
+        The method does not work because a status code is 
+        compared to a LogEntry. Instead, a for loop is 
+        needed to iterate over all the status codes of the 
+        LogEntrys in records to check to see if the 
+        statusCode is already in there. After looping, if 
+        it is not in there, then the LogEntry can be added 
+        to records. 
+        */
+    }
+
 }
--- 2.3.1. Reading Log Files/Tester.java
+++ 2.3.1. Reading Log Files/Tester.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class Tester {
 
     /**
-     * Test Case
+     * LogEntry Test Case
      */
     public void testLogEntry() {
         LogEntry le = new LogEntry("1.2.3.4", new Date(), "example request", 200, 500);
@@ -13,7 +13,7 @@ public class Tester {
     }
     
     /**
-     * Test Case
+     * Log Analyzer Test Case
      */
     public void testLogAnalyzer() {
 
@@ -27,13 +27,121 @@ public class Tester {
         log.printAll();
     }
 
+    /**
+     * uniqueIPs Test Case
+     */
+    public void testUniqueIP() {
+
+        // Creates a LogAnalyzer object
+        LogAnalyzer log = new LogAnalyzer();
+
+        // Read from the file
+        log.readFile("data\\short-test_log");
+
+        // Test the method countUniqueIPs
+        int uniqueIPs = log.countUniqueIPs();
+        System.out.println(uniqueIPs);
+    }
+
+    /**
+     * printAllHigherThanNum Test Case
+     */
+    public void testAllHigherThanNum() {
+
+        // Creates a LogAnalyzer object
+        LogAnalyzer log = new LogAnalyzer();
+
+        // Read from file
+        log.readFile("data\\short-test_log");
+
+        // Test the method
+        log.printAllHigherThanNum(200);
+    }
+
+    /**
+     * uniqueIPVisitsOnDay Test Case
+     */
+    public void testUniqueIPVisitsOnDay(String date, String filename) {
+
+        // Creates a LogAnalyzer object
+        LogAnalyzer log = new LogAnalyzer();
+
+        // Read from file
+        log.readFile(filename);
+        
+        // Calls method
+        ArrayList<String> addresses = log.uniqueIPVisitsOnDay(date);
+        System.out.println(addresses.size() + " Visits on " + date);
+
+    }
+
+    /**
+     * countUniqueIPsInRange Test Case
+     */
+    public void testUniqueIPsInRange(int low, int high, String filename) {
+
+        // Creates a LogAnalyzer object
+        LogAnalyzer log = new LogAnalyzer();
+
+        // Read from file
+        log.readFile(filename);
+
+        // Calls method
+        System.out.println(log.countUniqueIPsInRange(low, high));
+    }
+
+    /**
+     * readFileWithUniqueStatus Test Case
+     */
+    public void testFileWithUniqueStatus() {
+
+        // Creates a LogAnalyzer object
+        LogAnalyzer log = new LogAnalyzer();
+
+        // Calls readFile on the data file short-test_log
+        String filename = "data\\short-test_log";
+
+        // Calls method
+        log.readFileWithUniqueStatus(filename);
+
+        // Calls printAll to print all the web logs
+        log.printAll();
+    }
+    
     public static void main (String[] args ) {
         
         Tester tester = new Tester();
-
-        System.out.println("LogEntry ------------------");
+        /* 
+        */
+        System.out.println("------------------ LogEntry");
         tester.testLogEntry();
-        System.out.println("Analyzer ------------------");
+        
+        System.out.println("------------------ Analyzer");
         tester.testLogAnalyzer();
+        
+        System.out.println("------------------ Unique IPs");
+        tester.testUniqueIP();
+        
+        System.out.println("------------------ Status Code Higher Than 200");
+        tester.testAllHigherThanNum();
+        
+        System.out.println("------------------ IP Visits On Day");
+        // Test case 1, should return 2
+        tester.testUniqueIPVisitsOnDay("Sep 14", "data\\weblog-short_log");
+        // Test case 2, should return 3
+        tester.testUniqueIPVisitsOnDay("Sep 30", "data\\weblog-short_log");
+        // Quiz
+        tester.testUniqueIPVisitsOnDay("Mar 17", "weblog1_log");
+        
+        System.out.println("------------------ Unique IPs in Status Range");
+        // Test case 1, should return 4
+        tester.testUniqueIPsInRange(200, 299, "data\\short-test_log");
+        // Test case 2, should return 2
+        tester.testUniqueIPsInRange(300, 399, "data\\short-test_log");
+        // Quiz
+        tester.testUniqueIPsInRange(300, 399, "data\\weblog1_log");
+
+        System.out.println("------------------ Read File With Unique Status");
+        tester.testFileWithUniqueStatus();
     }
 }
